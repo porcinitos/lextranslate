e253081060af77d1b9fe06d359817cb41dea26c0
sam_pc <sam_pc@tuta.io>
2020-03-02 18:59:42 -0600

    estilos integrados & modulos de python en front

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,14 @@
 import eel
 
 
+@eel.expose
+def python_funct():
+  print("hola desde python")
 
-
-def __main__():
-  print("chepe")
 
 def __init__():
   eel.init('./gui')
-  eel.start('index.html')
+  eel.start('index.html', size = (960, 540), mode='chrome' )
 
 if __name__ == "__main__":
   __init__()
-  __main__()
--- src/gui/scripts/app.js
+++ src/gui/scripts/app.js
@@ -1,1 +1,2 @@
-console.log("chepe")
+
+eel.python_funct();

f8c44935c34904e492a61e05e7e4acf5b0777720
sam_pc <sam_pc@tuta.io>
2020-03-16 22:53:31 -0600

    iconos fueron agregados a la barra izquierda y cabecera

--- src/gui/scripts/app.js
+++ src/gui/scripts/app.js
@@ -1,2 +1,35 @@
+//elementos
+const app = document.getElementById("app")
+const entradaIzquierda = document.getElementById("entradaIzquierda")
+const entradaDerecha   = document.getElementById("entradaDerecha")
+const cabeceras = document.getElementById("cabeceras")
 
-eel.python_funct();
+// clases
+class ImportarRecursos{
+	constructor(){}
+	
+	async cargarRecursos(){
+		const microfono = await importarRecurso("../assets/iconos/micro.svg")
+		const bocina = await importarRecurso("../assets/iconos/bocina.svg")
+		const barraIzquierda = entradaIzquierda.getElementsByClassName("entrada__barra")[0]
+		let cambiar = await importarRecurso("../assets/iconos/cambiar.svg")
+    cambiar = document.createRange().createContextualFragment(cambiar) 
+		cabeceras.insertBefore(cambiar,cabeceras.lastElementChild)
+	  barraIzquierda.innerHTML += microfono + bocina;
+	}
+}
+
+// funciones
+async function importarRecurso(path){
+		const response = await fetch(path);
+		let text = await response.text();
+		return text;
+}
+
+(function events(){
+	document.addEventListener('DOMContentLoaded', () =>{
+		new ImportarRecursos().cargarRecursos();
+
+	})
+
+})();

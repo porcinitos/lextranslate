dc0c0ffec3c46b8a1a69f0308778037e893acc20
sam_pc <sam-pc0@users.noreply.github.com>
2020-05-20 09:06:18 -0600

    agregados elementos restantes del lenguaje

--- src/analizador/lexer.py
+++ src/analizador/lexer.py
@@ -1,8 +1,6 @@
 import ply.lex as lex
 import utils as U
 
-Sustantivos = []
-
 AdjetivoPosesivo = ['my', 'your' ,'his' ,'her' ,'its' ,'our', 'their']
 
 AdjetivoDemostrativo = ['this','that','these','those']
@@ -12,24 +10,21 @@ AdjetivoCalificativo = ['absent','minded','adventurous','affectionate','artistic
 'friendly','lazy','mean','nice','organized','clubbable','patient','polite','popular','famed','pop','pretty','serious','short','shy',
 'sincere','skinny','small','strange','strict','tall','thin','ugly','unorganized','worker','brave','dirty','confident','ecstatic',
 'energetic','afraid','ambitious','arrogant','ashamed','bad','cantankerous','careful','careless','conceited','confused','coward','cruel',
-'disappointed','disciplined','distressed','drunk','envious','quick','fast','flirtatious','good','gullible','healthy','jealous','lonely','Modest',
-'naughty','old','pessimistic','poor','possesive','practical','proud','rich','sad','sane','sick','slow','sober','strong','stubborn',
+'disappointed','disciplined','distressed','drunk','envious','quick','fast','flirtatious','good','gullible','healthy','jealous','lonely','Modest','naughty','old','pessimistic','poor','possesive','practical','proud','rich','sad','sane','sick','slow','sober','strong','stubborn',
 'stupid','sympathetic','trustworthy','weak','worried','young']
 
-AdjetivoNumeralCardinal = []
+AdjetivoNumeralCardinal = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty-one","twenty-two","twenty-three","twenty-four","twenty-five","twenty-six","twenty-seven","twenty-eight","twenty-nine","thirty","thirty-one","thirty-two","thirty-three","thirty-four","thirty-five","thirty-six","thirty-seven","thirty-eight","thirty-nine","forty","forty-one","forty-two","forty-three","forty-four","forty-five","forty-six","forty-seven","forty-eight","forty-nine","fifty","fifty-one","fifty-two","fifty-three","fifty-four","fifty-five","fifty-six","fifty-seven","fifty-eight","fifty-nine","sixty","sixty-one","sixty-two","sixty-three","sixty-four","sixty-five","sixty-six","sixty-seven","sixty-eight","sixty-nine","seventy","seventy-one","seventy-two","seventy-three","seventy-four","seventy-five","seventy-six","seventy-seven","seventy-eight","seventy-nine","eighty","eighty-one","eighty-two","eighty-three","eighty-four","eighty-five","eighty-six","eighty-seven","eighty-eight","eighty-nine","ninety","ninety-one","ninety-two","ninety-three","ninety-four","ninety-five","ninety-six","ninety-seven","ninety-eight","ninety-nine","one","hundred"]
 
-AdjetivoNumeralOrdinal = []
+AdjetivoNumeralOrdinal = ["first","seconds","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth","thirteenth","fourteenth","fifteenth","sixteenth","seventeenth","eighteenth","nineteenth","twentieth","twentyninth","thirtieth","thirtyfirst","thirtysecond","thirtythird","thirtyfourth","thirtyfifth","thirtysixth","thirtyseventh","thirtyeighth","thirtyninth","fortieth","fortyfirst","fortysecond","fortythird","fortyfourth","fortyfifth","fortysixth","fortyseventh","fortyeighth","fortyninth","fiftieth","fiftyfirst","fiftysecond","fiftythird","fiftyfourth","fiftyfifth","fiftysixth","fiftyseventh","fiftyeighth","fiftyninth","sixtieth","sixtyfirst","sixtysecond","sixtythird","sixtyfourth","sixtyfifth","sixtysixth","sixtyseventh","sixtyeighth","sixtyninth","seventieth","seventyfirst","seventysecond","seventythird","seventyfourth","seventyfifth","seventysixth","seventyseventh","seventyeighth","seventyninth","eightieth","eightyfirst","eightysecond","eightythird","eightyfourth","eightyfifth","eightysixth","eightyseventh","eightyeighth","eightyninth","ninetieth","ninetyfirst","ninetysecond","ninetythird","ninetyfourth","ninetyfifth","ninetysixth","ninetyseventh","ninetyeighth","ninetyninth","hundredth"]
 
 AdjetivoIndefinido = ['bad','worse','worst','ill','badly','far',' farther','farthest','further','furthest','better','best','well','late',
 'later','latest','little','less','least','many','more','most','much','some']
 
-ArticuloDefinido = []
+ArticuloDefinido = ["the"]
 
 ArticuloIndefinido = ['a','an']
 
-PronombresPersonales = ['I','you','we','they','she','he']
-
-PronombresNumerales= []
+PronombresPersonales = ['i','you','we','they','she','he']
 
 PronombreDemostrativo = ['such','none']
 
@@ -41,13 +36,21 @@ PronombrePosesivo = ['mine', 'yours', 'his', 'hers', 'ours', 'theirs']
 
 VerboToBe = ['am','is','are']
 
-AdverbioTiempo = []
+VerboPresente = ["like","answer","arrive","ask","be ","borrow","break ","buy ","catch ","clean","climb","collect","come ","compose","cook","cut ","dance","describe","discover","do ","drink ","drive ","eat ","enjoy","fall ","feel ","find ","fly ","forget ","give ","go ","happen","have ","help","hurt ","invent","invite","kill","know ","lend","leave ","lie ","like","live","look","love","make ","meet","miss","open","pack","pay ","phone","play","prefer","prepare","push","put ","rain","read ","remember","rent","rescue","return","ring ","save","say ","search","see ","sell ","sit ","skate","ski","sleep ","smell","speak","spend ","start","stay","stop","study","survive","swim ","take ","talk","teach","tell","think","throw","touch","try","understand","use","visit","wait","walk","want","wash","watch","wear ","work","write"]
 
-AdverbioLugar= []
+VerboPresenteParticipio = ["liking","answering","arriving","asking","been","borrowing","breaking","buying","catching","cleaning","climbing","collecting","comeing","composeing","cooking","cuting","dancing","describing","discovering","doing","drinking","driving","eating","enjoying","falling","feeling","finding","flying","forgetting","giving","going","happening","having","helping","hurting","inventing","inviteing","killing","knowing","lending","leaveing","lying","liking","living","looking","loving","making","meeting","missing","opening","packing","paying","phoning","playing","prefering","preparing","pushing","puting","raining","reading","remembering","renting","rescuing","returning","ringing","saving","saying","searching","seeing","selling","sitting","skating","skiing","sleeping","smelling","speaking","spending","starting","staying","stoping","studying","surviveing","swimming","taking","talking","teaching","telling","thinking","throwing","touching","trying","understanding","using","visiting","waiting","walking","wanting","washing","watching","wearing","working","writeing"]
+
+VerboPasado = ["liked","answered","arrived","asked","was","were","borrowed","broke","bought","caught","cleaned","climbed","collected","came","composed","cooked","cut","danced","described","discovered","did","drank","drove","ate","enjoyed","fell","felt","found","flew","forgot","gave","went","happened","had","helped","hurt","invented","invited","killed","knew","lent","left","lay","liked","lived","looked","loved","made","met","missed","opened","packed","paid","phoned","played","preferred","prepared","pushed","put","rained","read","remembered","rented","rescued","returned","rang","saved","said","searched","saw","sold","sat","skated","skied","slept","smelled","spoke","spent","started","stayed","stopped","studied","survived","swam","took","talked","taught","told","thought","threw","touched","tried","understood","used","visited","waited","walked","wanted","washed","watched","wore","worked","wrote"]
+
+VerboPasadoParticipio = ["been","broken","come","done","drunk","driven","eaten","fallen","flown","fallen","flown","gone","known","lain","rung","seen","spoken","swum","taken","thrown","worn","written"]
+
+AdverbioTiempo = ["currently","next","normally","now","meanwhile","never","last","night","eternally","occasionally","previously","finally","later","before","frequently","formerly","today","soon","regularly","initially","on","time","still","immediately","just","yesterday","instantly","recently","constantly","never","always","simultaneously","then","simultaneously","when","tomorrow","afternoon","While","early","after","momentarily","already"]
+
+AdverbioLugar= ["through","here","where","bottom","up","in","back","here","above","out","low","","front","there","close","between","edge","next","there","away","there","below","around","behind","about"]
 
 AdverbioCantidad = ['almost','enough','entirely','even','failry','hardly','lots','less','much','partially','pretty','rather','quite','scarcely','so','too','very','way']
 
-AdverbioModo= []
+AdverbioModo= ['slow','easy','simple','careful','natural','happy','clear','hard']
 
 AdverbioAfirmacion = ['certainly','clearly','indeed','naturally','obviously','surely','yes','definitely','obviously','really','truly','undoubtedly']
 
@@ -55,19 +58,15 @@ AdverbioNegacion = ['no','not','never','nowhere']
 
 AdverbioDuda = ['probably','maybe','perhaps','likely']
 
-AdverbioModo= ['slow','easy','simple','careful','natural','happy','clear','hard']
+Preposicion = ["to","before","low","fits","with","against","of","since","in","between","toward","until","for","by","according","without","on","after"]
 
-PreposicionTiempo = ['about','after','around','during','for','over','past','since','to','until','within']
+ConjuncionCoordinante = ['and','but','however','so','or','then','therefore','yet']
 
-PreposicionMovimiento = ['across','after','along','around','by','down','into','past','off','onto','to','from','toward','under','up']
-
-ConjugacionCoordinante = ['and','but','however','so','or','then','therefore','yet']
-
-ConjugacionSubordinantes = ['although','as','after','before','if','since','until', 'because','though','whereas','while','nevertheless','only','whether']
+ConjuncionSubordinantes = ['although','as','after','before','if','since','until', 'because','though','whereas','while','nevertheless','only','whether']
 
 Interjeccion = ['wow','yuck','ouch','uh','oops','hey','yeah','eh']
 
-Contracciones = []
+Contraccion = ["aight","ain't","amn't","aren't","can't","'cause","could've","couldn't","couldn't've","daren't","daresn't","dasn't","didn't","doesn't","don't","d'ye","e'er","everybody's","everyone's","finna","g'day","gimme","giv'n","gonna","gon't","gotta","hadn't","had've","hasn't","haven't","he'd","he'dn't've'd","he'll","he's","he've","how'd","howdy","how'll","how're","how's","I'd","I'd've","I'll","I'm","I'm'a","I'm'o","innit","I've","isn't","it'd","it'll","it's","let's","ma'am","mayn't","may've","methinks","mightn't","might've","mustn't","mustn't've","must've","needn't","ne'er","o'clock","o'er","ol'","oughtn't","'s","shalln't","shan't","she'd","she'll","she's","should've","shouldn't","shouldn't've","somebody's","someone's","something's","so're","that'll","that're","that's","that'd","there'd","there'll","there're","there's","these're","these've","they'd","they'll","they're","they've","this's","those're","those've","'tis","to've","'twas","wanna","wasn't","we'd","we'd've","we'll","we're","we've","weren't","what'd","what'll","what're","what's","what've","when's","where'd","where'll","where're","where's","where've","which'd","which'll","which're","which's","which've","who'd","who'd've","who'll","who're","who's","who've","why'd","why're","why's","willn't","won't","wonnot","would've","wouldn't","wouldn't've","y'all","y'all'd've","y'all'dn't've'd","y'all're","you'd","you'll","you're","you've","noun's"]
 
 Simbolos = {
         "," : "Signo_Coma" ,
@@ -89,38 +88,45 @@ Simbolos = {
         '_' : "Signo_guion_bajo",
         }
 
-tokens = ['Sujeto','PronombresPersonales','AdjetivoDemostrativo','PronombreObjeto','PronombrePosesivo','PronombreInterrogativo',
-'VerboToBe','AdjetivoPosesivo','AdjetivoCalificativo','AdjetivoIndefinido','ArticuloDefinido','ArticuloIndefinido','PronombreDemostrativo',
-'AdverbioCantidad','AdjetivodeModo','AdverbiodeAfirmacion','AdverbiodeNegacion','AdverbiodeDuda','PreposiciondeLugar',
-'PreposiciondeTiempo','ConjugacionCoordinante','PreposiciondeMovimiento','ConjugacionSubordinantes','Interjeccion'] +[*Simbolos.values()]
+tokens = ['Sustantivo','AdjetivoPosesivo','AdjetivoDemostrativo','AdjetivoCalificativo','AdjetivoNumeralCardinal','AdjetivoNumeralOrdinal','AdjetivoIndefinido','ArticuloDefinido','ArticuloIndefinido','PronombresPersonales','PronombreDemostrativo','PronombreInterrogativo','PronombrePosesivo','PronombreObjeto','VerboToBe', 'VerboPresente', 'VerboPresenteParticipio', 'VerboPasado', 'VerboPasadoParticipio','AdverbioTiempo','AdverbioLugar','AdverbioCantidad','AdverbioModo','AdverbioAfirmacion','AdverbioNegacion','AdverbioDuda','Preposicion', 'ConjuncionCoordinante','ConjuncionSubordinantes','Interjeccion','Contraccion'] +[*Simbolos.values()]
 
 
-def t_Sujeto(t):
+def t_Sustantivo(t):
   '''
   [a-zA-z]+
   '''
-  if (t.value in PronombresPersonales): t.type = "PronombresPersonales"
+  t.value = t.value.lower()
+  if   (t.value in AdjetivoPosesivo): t.type = "AdjetivoPosesivo"
   elif (t.value in AdjetivoDemostrativo): t.type = "AdjetivoDemostrativo"
+  elif (t.value in AdjetivoCalificativo): t.type = "AdjetivoCalificativo"
+  elif (t.value in AdjetivoNumeralCardinal): t.type = "AdjetivoNumeralCardinal"
+  elif (t.value in AdjetivoNumeralOrdinal): t.type = "AdjetivoNumeralOrdinal"
+  elif (t.value in AdjetivoIndefinido): t.type = "AdjetivoIndefinido"
+  elif (t.value in ArticuloDefinido): t.type = "ArticuloDefinido"
+  elif (t.value in ArticuloIndefinido): t.type = "ArticuloIndefinido"
+  elif (t.value in PronombresPersonales): t.type = "PronombresPersonales"
+  elif (t.value in PronombreDemostrativo): t.type = "PronombreDemostrativo"
+  elif (t.value in PronombreInterrogativo): t.type = "PronombreInterrogativo"
   elif (t.value in PronombreObjeto): t.type = "PronombreObjeto"
   elif (t.value in PronombrePosesivo): t.type = "PronombrePosesivo"
-  elif (t.value in PronombreInterrogativo): t.type = "PronombreInterrogativo"
   elif (t.value in VerboToBe): t.type = "VerboToBe"
-  elif (t.value in AdjetivoPosesivo): t.type = "AdjetivoPosesivo"
-  elif (t.value in AdjetivoCalificativo): t.type = "AdjetivoCalificativo"
-  elif (t.value in AdjetivoIndefinido): t.type = "AdjetivoIndefinido"
-  elif (t.value in ArticuloIndefinido): t.type = "ArticuloIndefinido"
-  elif (t.value in PronombreDemostrativo): t.type = "PronombreDemostrativo"
+  elif (t.value in VerboPresente): t.type = "VerboPresente"
+  elif (t.value in VerboPresenteParticipio): t.type = "VerboPresenteParticipio"
+  elif (t.value in VerboPasado): t.type = "VerboPasado"
+  elif (t.value in VerboPasadoParticipio): t.type = "VerboPasadoParticipio"
+  elif (t.value in AdverbioTiempo): t.type = "AdverbioTiempo"
+  elif (t.value in AdverbioLugar): t.type = "AdverbioLugar"
   elif (t.value in AdverbioCantidad): t.type = "AdverbioCantidad"
-  elif (t.value in AdjetivodeModo): t.type = "AdjetivodeModo"
-  elif (t.value in AdverbioAfirmacion): t.type = "AdverbiodeAfirmacion"
-  elif (t.value in AdverbioNegacion): t.type = "AdverbiodeNegacion"
-  elif (t.value in AdverbioDuda): t.type = "AdverbiodeDuda"
-  elif (t.value in PreposiciondeLugar): t.type = "PreposiciondeLugar"
-  elif (t.value in PreposiciondeTiempo): t.type = "PreposiciondeTiempo"
-  elif (t.value in ConjugacionCoordinante): t.type = "ConjugacionCoordinante"
-  elif (t.value in PreposiciondeMovimiento): t.type = "PreposiciondeMovimiento"
-  elif (t.value in ConjugacionSubordinantes): t.type = "ConjugacionSubordinantes"
+  elif (t.value in AdverbioModo): t.type = "AdverbioModo"
+  elif (t.value in AdverbioAfirmacion): t.type = "AdverbioAfirmacion"
+  elif (t.value in AdverbioNegacion): t.type = "AdverbioNegacion"
+  elif (t.value in AdverbioDuda): t.type = "AdverbioDuda"
+  elif (t.value in Preposicion): t.type = "Preposicion"
+  elif (t.value in ConjuncionCoordinante): t.type = "ConjuncionCoordinante"
+  elif (t.value in ConjuncionSubordinantes): t.type = "ConjuncionSubordinantes"
+  elif (t.value in Preposicion): t.type = "Preposicion"
   elif (t.value in Interjeccion): t.type = "Interjeccion"
+  elif (t.value in Contraccion): t.type = "Contraccion"
   return t
 
 
@@ -145,4 +151,4 @@ def scan(txt):
     if not tok : break
     print(":: %s \n" % tok)
 
-scan("this is my favorite game")
+scan("I like to play with my girlfriend")

e2ab3731366149c8a989656be83dbc503b730ef0
fersincorado <fersincorado@users.noreply.github.com>
2020-05-22 14:21:23 -0600

    refactorice

--- src/analizador/utils.py
+++ src/analizador/utils.py
@@ -5,8 +5,6 @@ def guardarError(t,tipo):
   global tablaErrores
   tablaErrores += tipo+"('%s',%s,%s)\n" % (t.value, t.lineno,     t.lexpos)
 
-
-
 def guardarToken(t):
   global tablaSimbolos
   tablaSimbolos += "Simbolo (%s,%s,%s)\n" % (id,t.type,t.value)
